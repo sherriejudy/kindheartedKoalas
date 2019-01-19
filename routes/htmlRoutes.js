a84d3b1971ebff4e51e08e66ed41feb38c8ba6c4
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2019-01-19 13:15:40 -0500

    fixed index view

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,10 +4,9 @@ module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
       res.render("index", {
-        msg: "Welcome!"
+        msg: "Welcome to kPark!"
       });
     });
-  });
 
 
 
