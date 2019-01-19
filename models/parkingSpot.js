70570f7fc389dfcc709f115ab0244f171e306780
Tahreem <tahreemsohailbutt@gmail.com>
2019-01-19 14:31:44 -0500

    partially updated parkingSpot table

--- models/parkingSpot.js
+++ models/parkingSpot.js
@@ -1,7 +1,39 @@
 module.exports = function(sequelize, DataTypes) {
   var parkingSpot = sequelize.define("parkingSpot", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+
+    address: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+
+    image_reference: DataTypes.STRING,
+
+    rate_dollar: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+
+    rate_unit: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    
+    
+    
+    DataTypes.STRING,
+    availability: DataTypes.BOOLEAN,
+    lease_terms: DataTypes.TEXT,
+    spot_size: DataTypes.STRING
+
   });
   return parkingSpot;
 };
