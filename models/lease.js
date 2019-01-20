f7f17149f6778c5bfe1fd284afbc68554b22c43a
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2019-01-20 15:02:31 -0500

    lease model

---
+++ models/lease.js
@@ -0,0 +1,9 @@
+module.exports = function(sequelize, DataTypes) {
+  var lease = sequelize.define("lease", {
+    first_name_leasor: DataTypes.STRING,
+    last_name_leasor: DataTypes.STRING, 
+    from_date: DataTypes.DATE, 
+    to_date: 
+  });
+  return parkingSpot;
+};
