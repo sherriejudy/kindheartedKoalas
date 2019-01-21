107bb9745432969657521e7b6f8adf724ecd8b16
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2019-01-21 16:14:02 -0500

    updated html get route to find listings with addresses

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,28 +3,22 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.parkingSpot.findAll({}).then(function(data){
+    db.parkingSpot.findAll({
+      include:[{ 
+        model: address
+      }]
+    })
+    .then(function(data){
       res.render("spotListing", {
         msg: 'test',
         parkingSpots: data 
       })
       })
-    })  
-  
-
-
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+    })
+    
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
-};
+}
