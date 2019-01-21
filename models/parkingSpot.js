1360f327afcdaadc0d16ea0de848eb04d5b5bd65
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2019-01-21 13:56:25 -0500

    removed address field from parkingSpot model

--- models/parkingSpot.js
+++ models/parkingSpot.js
@@ -1,12 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
   var parkingSpot = sequelize.define("parkingSpot", {
-    address: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
     image_reference: DataTypes.STRING,
     rate_dollar: {
       type: DataTypes.STRING,
