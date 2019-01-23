3b5f367f3ddfca46d57c721087b71f3e57841f14
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2019-01-22 20:40:08 -0500

    got a map of toronto

--- public/js/location.js
+++ public/js/location.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
         var service;
         var markers = []; //array to be filled with marker objects created by the createMarker function
 
-        function initialize() {
+        function initMap() {
             var center = new google.maps.LatLng(43.660781, -79.396785);
             map = new google.maps.Map(document.getElementById('coffee-map'), {
                 center: center,
@@ -58,61 +58,14 @@ $(document).ready(function(){
         }
 
 
-        function createMarker(place) { 
-            var marker = new google.maps.Marker({ //creates the actual markers for placement on the map 
-                map: map,
-                position: place.geometry.location
-            });
 
-            google.maps.event.addListener(marker, 'click', function(){ //add a listener to each marker on creation so clicking on it will open an info bubble
-
-                var isOpen; // a string set by this if-else based on google's "open_now" boolean in the "opening hours" key of the place object
-                    if (place.opening_hours.open_now == true) 
-                    {
-                        isOpen = "Open now." // the message that will go in the info-bubble if the place is open
-                    }
-                    else 
-                    {
-                        isOpen = "Closed now." // ditto, if closed
-                    }
-                
-                $("#coffee-destination").val(place.name + " (" + place.vicinity + ")"); //when the marker for a shop is clicked, the destination form input automatically fills with the name of the shop
-                console.log(place.name + " selected.");
-
-                var placeData = [place.name + ", </br>", place.vicinity + "</br>", isOpen]; // short array of data we want to show the user about the cafe when it's clicked
-                infoBubble.setContent(placeData[0] + placeData[1] + placeData[2]); //set the bubble to show the cafe's name, address and open/closed status
-                infoBubble.open(map, this);
-                console.log(place);
-                
-            });
-            return marker;
-        }
-
-        function createMyhalMarker(){ //function to set a unique marker for the myhal building at center of map
-
-            var myhalMarker = new google.maps.Marker({
-                position:{lat:43.660781,lng:-79.396785},
-                map: map,
-                icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png' //a flag, instead of the standard marker icon
-            });
-
-            google.maps.event.addListener(myhalMarker, 'click', function(){ //add a listener to this marker on creation so clicking on it will open an info bubble
-
-                infoBubble.setContent("Myhal Centre For Engineering</br> Innovation & Entrepreneruship, </br> 55 St. George St, Toronto ON"); //bubble hard-coded with name && address
-                infoBubble.open(map, this);
-            });
-        }
+    
+        
     
 
-        function clearResults(markers) { //clears the markers array for re-populating with new markers when a new center is set
-            for (var m in markers) {
-                markers[m].setMap(null)
-            }
-            markers = [];
-        }
+        
 
 
-        initialize(); //function called on page load
-        createMyhalMarker();
+        
 
 }); //end of docready function
