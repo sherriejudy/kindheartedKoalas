74ed8e19aedae988a9a66c70a01803bd149c094e
Judy Hu <judy.hu9@gmail.com>
2019-01-24 23:08:11 -0500

    fixed merge conflicts

--- public/js/vendorInput.js
+++ public/js/vendorInput.js
@@ -1,21 +1,15 @@
-<<<<<<< HEAD
-var To = $("[name='to']")
-console.log(To)
-// Do the lat/long conversion here
-=======
 // var To = $("[name='to']");
 // console.log(To);
 
 
 
 
-$(".create-form").submit(function(event){
+$(".create-form").submit(function (event) {
 
     event.preventDefault();
     console.log($(".create-form").serializeArray());
 
-}); 
-    
+});
 
 
->>>>>>> 76e16856bcc29df1dfd4cb66ac2de1716a9e09d5
+
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,28 +1,16 @@
 var db = require("../models");
 
 module.exports = function (app) {
-<<<<<<< HEAD
   // render html page that displays single listing 
-  app.get("/api/findSpot/:id", function (req, res) {
-    console.log(req.params.id)
-    db.parkingSpot.findById(req.params.id, {
-      include: [db.lease, db.address]
-    })
-      .then(function (spotSelected) {
-        res.render('index',
-          { spotSelected: spotSelected })
-=======
-// render html page that displays single listing 
   app.get("/findSpot/:id", function (req, res) {
     db.parkingSpot.findById(req.params.id, {
       include: [db.lease, db.address]
     })
       .then(function (data) {
-        res.render('spotDetail',{
+        res.render('spotDetail', {
           parkingSpot: data,
           layout: 'mainmap'
         })
->>>>>>> ed855720a1664f4a074c364d8c9d24404164e9d9
       })
 
   });
