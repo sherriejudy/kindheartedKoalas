10e67c87f180d750d4065a9afcb05cfc19835d2d
Tahreem <tahreemsohailbutt@gmail.com>
2019-01-25 21:43:34 -0500

    what i currently have: confirmation page won't load on redirect, not sure why right now

--- public/js/vendorInput.js
+++ public/js/vendorInput.js
@@ -36,14 +36,16 @@ $(".create-form").submit(function (event) {
         aboutYourself: aboutYourself
     };
 
-    console.log(JSON.stringify({
-        leaseDetails: leaseDetails,
-        parkingSpotDetails: parkingSpotDetails,
-        aboutYourself: aboutYourself
-    }));
+    var address = String(parkingSpotDetails.streetNumber)+" "+String(parkingSpotDetails.streetName)+" "+String(parkingSpotDetails.city);
+
+    // console.log(JSON.stringify({
+    //     leaseDetails: leaseDetails,
+    //     parkingSpotDetails: parkingSpotDetails,
+    //     aboutYourself: aboutYourself
+    // }));
 
     // adding geocode call
-    $.get('/api/geocode?address='+)
+    $.get('/api/geocode?address='+'5125 Celebration Dr, ON, Canada') //trying this instead of the var address temporarily
     .then(function(data,err){
         $.ajax('http://localhost:3000/vendorInput', {
         type: 'POST',
@@ -56,19 +58,36 @@ $(".create-form").submit(function (event) {
         success: function () {
             console.log('success');
             // Redirect
-            window.location.href = "/vendorConfirmation";
-            vendorConfirmation(data);
+            // window.location.href = "/vendorConfirmation";
+            // vendorConfirmation(data);
+
+
+            $.ajax('http://localhost:3000/vendorConfirmation', {  //trying this instead of the same call from vendorConfirmation(data) on line 62 to see if this will work 
+                type: 'GET',
+                data: JSON.stringify(data),
+                contentType: 'application/json',
+                success: function () {
+                    console.log('success');
+                    console.log("MEOWWWW   "+data);
+                    $(".container").text(data);
+                    // Redirect
+                    //window.location.href =;
+                },
+                error: function () { console.log('error'); }
+            });
+
+
+
+
         },
         error: function () { console.log('error'); }
     });
 
-    })
+    });
     
 
-    console.log(JSON.stringify(leaseDetails) + "\n" + JSON.stringify(parkingSpotDetails) + " \n " + JSON.stringify(aboutYourself));
+    //console.log(JSON.stringify(leaseDetails) + "\n" + JSON.stringify(parkingSpotDetails) + " \n " + JSON.stringify(aboutYourself));
 
 
 });
-
-
 
